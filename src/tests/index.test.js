b1da8c971d79de748e6c7bbe6b967461d9ba9017
LEESAROY <42925369+LEESAROY@users.noreply.github.com>
2024-12-09 22:45:23 -0500

    made changes

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,27 +1,20 @@
-const lambda = require('../index');
+const { handler } = require('../../index');
 
-describe('Lambda Function', () => {
-    it('should return a 200 status code with correct message', async () => {
-        const event = {
-            path: '/api/data',
-            httpMethod: 'GET'
-        };
-        
-        const result = await lambda.handler(event);
-        
-        expect(result.statusCode).toBe(200);
-        expect(JSON.parse(result.body).message).toBe("Hello from Final project");
-    });
+test('Returns message from /api/data endpoint', async () => {
+    const event = {
+        path: '/api/data',
+        httpMethod: 'GET'
+    };
+    const result = await handler(event);
+    expect(result.statusCode).toBe(200);
+    expect(JSON.parse(result.body).message).toBe("Hello from Final project");
+});
 
-    it('should return a 404 status code for unknown path', async () => {
-        const event = {
-            path: '/unknown',
-            httpMethod: 'GET'
-        };
-        
-        const result = await lambda.handler(event);
-        
-        expect(result.statusCode).toBe(404);
-    });
-
+test('Returns 404 for unknown endpoint', async () => {
+    const event = {
+        path: '/unknown',
+        httpMethod: 'GET'
+    };
+    const result = await handler(event);
+    expect(result.statusCode).toBe(404);
 });
