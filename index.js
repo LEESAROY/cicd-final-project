53ec7b67bfa8063732713d44915157a13916f6f2
LEESAROY <42925369+LEESAROY@users.noreply.github.com>
2024-12-11 01:44:54 -0500

    update

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 exports.handler = async (event) => {
     console.log('Event:', JSON.stringify(event, null, 2));
 
-    if (event.path === '/api' && event.httpMethod === 'GET') {
-        const response = {
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type,Authorization'
+    };
+
+    if (event.httpMethod === 'OPTIONS') {
+        return {
             statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'GET,OPTIONS',
-                'Access-Control-Allow-Headers': 'Content-Type,Authorization'
-            },
+            headers: headers,
+            body: JSON.stringify({ message: "CORS check successful" })
+        };
+    }
+
+    if (event.path === '/api' && event.httpMethod === 'GET') {
+        return {
+            statusCode: 200,
+            headers: headers,
             body: JSON.stringify({ message: "Hello from Final project" })
         };
-        return response;
     }
 
     return {
         statusCode: 404,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET,OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type,Authorization'
-        },
+        headers: headers,
         body: JSON.stringify({ message: "Not Found" })
     };
 };
