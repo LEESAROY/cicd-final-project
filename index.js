93fe1eab9cec613d85b6b7078ec03da1eeace76f
LEESAROY <42925369+LEESAROY@users.noreply.github.com>
2024-12-11 01:34:25 -0500

    update

--- index.js
+++ index.js
@@ -4,13 +4,23 @@ exports.handler = async (event) => {
     if (event.path === '/api' && event.httpMethod === 'GET') {
         const response = {
             statusCode: 200,
-            body: JSON.stringify({ message: "Hello from Final project" }),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type,Authorization'
+            },
+            body: JSON.stringify({ message: "Hello from Final project" })
         };
         return response;
     }
 
     return {
         statusCode: 404,
-        body: JSON.stringify({ message: "Not Found" }),
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET,OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type,Authorization'
+        },
+        body: JSON.stringify({ message: "Not Found" })
     };
 };
